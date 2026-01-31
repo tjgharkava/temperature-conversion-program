60ef977e1d87fea62b1d6bdbee13cc6d2560b9a7
Temo Jgarkava <tdzhgarkava@yahoo.com>
2026-01-31 22:43:31 +0400

    convert fahrenheit to celsius

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ function convert() {
     if(toFahrenheit.checked) {
         temp = Number(textBox.value);
         temp = temp * 9 / 5 + 32;
-        result.textContent = temp + "°F"
+        result.textContent = temp.toFixed(1) + "°F";
     } else if(toCelsius.checked) {
-        
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(2) + "°C";
     } else {
         result.textContent = "Select a unit!";
     }
